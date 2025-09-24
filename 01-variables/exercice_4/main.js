d062ac6cc33f13602fb78dee6a9927e13768eec2
Muks <Muks08325@gmail.com>
2025-09-24 11:08:02 +0100

    Added a commentary in the 01-variables/exercice_4/main.js and declare two variables a and b, give them the values ​​5 and 8, then display their sum with console.log.

--- 01-variables/exercice_4/main.js
+++ 01-variables/exercice_4/main.js
@@ -1,2 +1,6 @@
 // Exercice 4
-// Écrivez votre code ici
+// Déclare deux variables a et b, donne-leur les valeurs 5 et 8, puis affiche leur somme avec console.log.
+let a = 5;
+let b = 8;
+
+console.log(a + b);
