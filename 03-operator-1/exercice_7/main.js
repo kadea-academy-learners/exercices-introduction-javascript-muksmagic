30e7680d67dc982016291bdeca764a3a5a904e34
Muks <Muks08325@gmail.com>
2025-09-24 18:07:24 +0100

    feat: add commentary in 03-operator-1/exercice_7/main.js and add variables for family ages (child, father, mother, grandfather, uncle)

--- 03-operator-1/exercice_7/main.js
+++ 03-operator-1/exercice_7/main.js
@@ -1,2 +1,18 @@
 // Exercice 7
-// Écrivez votre code ici
+// Add variables for family ages and calculate relationships (child, father, mother, grandfather, uncle)
+
+let ageEnfant = 10;
+
+let agePere = ageEnfant * 2;
+
+let ageMere = agePere - 5;
+
+let ageGrandPere = (ageMere * 2) + (ageEnfant / 2);
+
+let ageOncle = agePere + 10;
+
+console.log("Âge de l'enfant :", ageEnfant);
+console.log("Âge du père :", agePere);
+console.log("Âge de la mère :", ageMere);
+console.log("Âge du grand-père :", ageGrandPere);
+console.log("Âge de l'oncle :", ageOncle);
