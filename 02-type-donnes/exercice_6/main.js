c10868bd71a0a1cf1fb383af28a9406e80c9049f
Muks <Muks08325@gmail.com>
2025-09-24 18:53:09 +0100

    Updated: a commentary in the 02-type-donnes/exercice_6/main.js and Declare variables with different types of values: number, string, boolean, null, and undefined

--- 02-type-donnes/exercice_6/main.js
+++ 02-type-donnes/exercice_6/main.js
@@ -1,9 +1,18 @@
 // Exercice 6
-// // Declare variables with different types of values: number, string, boolean, null, and undefined
+// Declare variables with different types of values: number, string, boolean, null, and undefined
 
 let age = 20;
 let nom = "Muks";
 let estConnecte = true;
 let valeurNulle = null;
-let inconnue;
+let inconnue; // undefined
+
 console.log(age, nom, estConnecte, valeurNulle, inconnue);
+
+module.exports = {
+    age,
+    nom,
+    estConnecte,
+    valeurNulle,
+    inconnue
+};
