ffc6a0cb656b66e28fa261577621c1efa2d5151d
Muks <Muks08325@gmail.com>
2025-09-24 18:43:59 +0100

    Updated: implement inheritance distribution for Jean Mukuna's estate (calculate shares for children, spouse, siblings, and representatives)

--- 03-operator-1/exercice_9/main.js
+++ 03-operator-1/exercice_9/main.js
@@ -5,30 +5,23 @@ let maison = 60000000;
 let terrains = 40000000;
 let liquidites = 20000000;
 
-let heritageTotal = maison + terrains + liquidites;
+let heritageTotal = 120000000;
 
 let partEnfants = heritageTotal * 0.75;
-
-let partConjointEtFreres = heritageTotal * 0.25;
-
-
 let nbPartsEnfants = 3;
 let partParEnfant = partEnfants / nbPartsEnfants;
 
 let paul = partParEnfant;
-
 let marie = partParEnfant;
+let eric = partParEnfant / 2;
+let claire = partParEnfant / 2;
 
-let partAlain = partParEnfant;
-let eric = partAlain / 2;
-let claire = partAlain / 2;
-
+let partConjointEtFreres = heritageTotal * 0.25;
 let nbPartsDeuxiemeCat = 3;
-let partParPersonneDeuxiemeCat = partConjointEtFreres / nbPartsDeuxiemeCat;
 
-let madameMukuna = partParPersonneDeuxiemeCat;
-let joseph = partParPersonneDeuxiemeCat;
-let sarah = partParPersonneDeuxiemeCat;
+let madameMukuna = partConjointEtFreres / nbPartsDeuxiemeCat;
+let joseph = partConjointEtFreres / nbPartsDeuxiemeCat;
+let sarah = partConjointEtFreres / nbPartsDeuxiemeCat;
 
 console.log("Répartition de l'héritage :");
 console.log("Paul :", paul.toLocaleString("fr-CD"), "CDF");
@@ -38,3 +31,5 @@ console.log("Claire :", claire.toLocaleString("fr-CD"), "CDF");
 console.log("Madame MUKUNA :", madameMukuna.toLocaleString("fr-CD"), "CDF");
 console.log("Joseph :", joseph.toLocaleString("fr-CD"), "CDF");
 console.log("Sarah :", sarah.toLocaleString("fr-CD"), "CDF");
+
+module.exports = { paul, marie, eric, claire, madameMukuna, joseph, sarah };
